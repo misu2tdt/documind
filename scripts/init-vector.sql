db93140d06e65c67d8599d1b164a585315b96390
Tien <tdtien7a1@gmail.com>
2026-08-16 01:30:20 +0700

    chore(db): remove legacy vector initialization script

--- scripts/init-vector.sql
+++
@@ -1,7 +0,0 @@
-ALTER TABLE chunks
-  ALTER COLUMN embedding TYPE vector(1536)
-  USING embedding::vector;
-
-CREATE INDEX IF NOT EXISTS chunks_embedding_idx
-  ON chunks
-  USING hnsw (embedding vector_cosine_ops);
